12fb411435bcc0dc0d35038a57f3e31594f33713
Alex Fuss <alexfuss2@gmail.com>
2020-06-12 16:21:39 -0400

    Changed up the script to include prompts for the user to customize the random password.

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # password-generator
 https://alexfuss.github.io/password-generator 
-The provided document allows a user to generate a random password with 15 characters. The password will also always include a random set of lowercase, uppercase, numeric and special characters. 
+The provided document allows a user to generate a random password by simply answering a few questions in regards to what he or she wants included. Following answering each prompt, the user simply clicks the "Get Password" button to get the result.  
 
 # layout
-The same bootstrap layout and navigation were kept for consistency sake. The navigation that lives within the portolio project pages (about, portfolio, contact, etc.) has been included. 
+The layout ws built using bootstrap. The navigation was pulled from some of my previous work for the sake of both efficiency and consistency. 
+
+# languages 
+- HTML
+- CSS
+- Bootstrap
+- JavaScript
+- Git
--- script.js
+++ script.js
@@ -1,19 +1,52 @@
 var generateBtn = document.querySelector("#generate");
+var lowerChar = "abcdefghijklmnopqrstuvwxyz";
+var capChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var specialChar = "!$^&*-=+_?";
+var newPass = [];
 
-function generatePassword() {
-    var length = 15;
-    var randomChar = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-    var returnValue = "";
-    for (var i = 0, n = randomChar.length; i < length; ++i) {
-        returnValue += randomChar.charAt(Math.floor(Math.random() * n));
+var passLength =  parseInt(prompt("How many characters do you want in your password?"));
+
+if (passLength < 6) {
+  alert("All passwords must be between 6-100 characters.");
+  prompt("How many characters do you want in your password?");
+  
+ }
+ else if (passLength > 6 ); {
+
+ }
+var upperCase = confirm("Do you want uppercase characters?"); 
+var lowerCase = confirm("Do you want lowercase characters?"); 
+var numVal = confirm("How about some numbers?"); 
+var specCase = confirm("Probably want special characters too right?");
+
+    if (upperCase === true) {
+      (newPass.push(capChar));
     }
-    return returnValue;
+    if (lowerCase === true) {
+      (newPass.push(lowerChar));
+    }
+    if (numVal === true) {
+      (newPass.push(numbers));
+    }
+    if (specCase === true) {
+      (newPass.push(specialChar));
+    }
+
+var newPassWord = newPass.join("");
+
+var password = "";
+
+for (var i = 0; i < passLength; i++) {
+  var finalPass = newPassWord.charAt(Math.floor(Math.random() * newPassWord.length));
+  password = password.concat(finalPass);
+  
+  }
+  
+alert("Click to get your password!");
+  
+function generatePassword() {
+  document.querySelector(".password").textContent = password;
 }
 
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector(".password");
-  passwordText.value = password;
-}
-
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
